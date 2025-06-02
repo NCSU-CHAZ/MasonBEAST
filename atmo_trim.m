2cec3106ec179c372d9638d65530dedbf14cea8b
bgaenzle <bagaenzl@ncsu.edu>
2025-06-02 11:11:55 -0400

    function to trim and interpolate atmospheric pressure data

--- atmo_trim.m
+++ atmo_trim.m
@@ -1,4 +1,4 @@
-function[trimmed_atmos]=atmo_trim(atmospress, atmostime,RBR_structure,start_time,end_time,savepath)
+function[Patmos]=atmo_trim(atmospress, atmostime,RBR_structure,start_time,end_time)
 %function[trimmed_atmos]=atmo_trim(atmospress,atmostime,RBR_structure,start_time,end_time,savepath)
 % 
 % This function takes in atmospheric pressure data and converts it to dbar,
@@ -12,11 +12,28 @@ function[trimmed_atmos]=atmo_trim(atmospress, atmostime,RBR_structure,start_time
 % RBR_structure = structure from RSKreaddata
 % start_time = collection start time in string format 'yyyy-mm-dd-HH-MM'
 % end_time = collection end time in string format 'yyyy-mm-dd-HH-MM'
-% savepath = path to save trimmed atmos data
 % 
 % OUTPUTS:
 % ---------
-% trimmed_atmos = vector of trimmed, linearly interpolated atmospheric
+% Patmos = vector of trimmed, linearly interpolated atmospheric
 % pressure data in dbar
 
+% Constants
+mbar2dbar=0.01; 
 
+% Convert start and end time to dateime
+format_in='yyyy-mm-dd-HH-MM';
+start_time=datenum(start_time,format_in);
+end_time=datenum(end_time,format_in);
+
+
+% Trim to start and end times
+time_trim=find(atmostime>=start_time & atmostime<= end_time);
+atmostime=atmostime(time_trim);
+atmospress=atmospress(time_trim);
+
+% convert from mbar to dbar
+atmospress=atmospress.*mbar2dbar;
+
+% linearly interpolate vector to be the same length as RBR pressure data
+Patmos = interp1(atmospress,RBR_structure.data.values);
