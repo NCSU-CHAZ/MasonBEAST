dfa10a9a2e12672fe9b09030e2e9b8a3fc32287c
bgaenzle <bagaenzl@ncsu.edu>
2025-06-02 11:42:07 -0400

    added functionality

--- window_data.m
+++ window_data.m
@@ -1,2 +1,26 @@
-function[win_data]=window_data(fs,window_time,data)
-% function[win_data]=window_data(fs,window_time,data)
+function[win_data]=window_data(fs,window_length,data)
+% function[win_data]=window_data(fs,window_length,data)
+% 
+% This function takes in a data array of one column of a creates a matrix
+% of data, where each column represents a window of data.
+% 
+% INPUTS:
+% --------
+% fs = sampling rate (Hz)
+% window_length = length of window (s)
+% data = array of data (one column)
+% 
+% OUTPUTS:
+% ---------
+% win_data = windowed data matrix
+% 
+
+window=window_length*fs; 
+nbursts = floor(length(data)/window); % number of bursts in datasets (rounded to nearest whole number)
+win_data= deal(NaN(window,nbursts)); % preallocate array
+
+windct=0;
+for i=1:nbursts
+    win_data(:,i)=data((1+windct):(windct+window));
+    windct=windct+window; % increase counter
+end
