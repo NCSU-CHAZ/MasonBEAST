4ad6331d9dffc4bd930ab774758a9ffeb9fc05bd
bgaenzle <bagaenzl@ncsu.edu>
2025-06-03 09:35:06 -0400

    save guage pressure

--- TC8_process.m
+++ TC8_process.m
@@ -1,5 +1,6 @@
 %% TC8 Processing Script 
 % BG 06/02/2025
+% This script reads atmospheric and RBR data and saves RBR guage data.
 
 % Paths
 addpath('/Users/bagaenzl/Desktop/rbr-rsktools-7a76410a599a');
@@ -21,19 +22,21 @@ taper_type='Slepian'; % slepian tapers
 RBR_structure=RSKopen(RBRpath);
 RBR_structure=RSKreaddata(RBR_structure); 
 
-atmos=readtable(atmospath);
+atmos=readtable(atmospath,'NumHeaderLines',6);
 atmospress=table2array(atmos(:,2)); % atmos pressure in dbar every 6 minutes
 atmostime=table2array(atmos(:,1));
 
-
-% Trim and interpolate atmospheric pressure data
+% Trim and interpolate atmospheric pressure data, save
 start_time='2024-09-05-16-48-15'; % sept 5 2024, 4:36:15 PM (start of bucket test)
 end_time='2024-09-18-17-22-00'; % sept 18 2024, 5:22:00 PM (end of final bucket test)
 [Patmos]=atmo_trim(atmospress,atmostime,RBR_structure,start_time,end_time);
+clear atmos atmospress atmostime;
 
-% Calculate guage pressure
+% Calculate guage pressure and save
 RBR_structure=RSKderiveseapressure(RBR_structure,'patm',Patmos); % (remove atmospheric pressure)
 RBR_pressure = RBR_structure.data.values(:,2).*dbar2Pa; % guage pressure (Pa)
+RBR_gpress_filepath=fullfile(savepath,[append('RBR_guage_09052024_09182024','.mat')]);
+save(RBR_gpress_filepath,"RBR_pressure") ;
 
 % Calculate pressure power spectra
 [P_window,nburst,NFFT]=window_data(fs,window_length,RBR_pressure); % window guage pressure data
