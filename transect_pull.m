05d709b95e615ae67433fd95cda9152ca7793c71
bgaenzle <bagaenzl@ncsu.edu>
2025-06-05 11:19:28 -0400

    pull transect for each frame of GEM given and save as .mat and .png

--- transect_pull.m
+++ transect_pull.m
@@ -1,4 +1,4 @@
-function[ztran]=transect_pull(GEMpath,yavg,dxy,ypick,savepath,figpath)
+function[ztran]=transect_pull(GEMpath,numframes,yavg,dxy,ypick,savepath,figpath)
 % function[ztran]=transect_pull(GEMpath,dxy,transect_loc)
 % 
 % This function pulls a transect for each GEM given and saves it as a mat
@@ -6,7 +6,8 @@ function[ztran]=transect_pull(GEMpath,yavg,dxy,ypick,savepath,figpath)
 % 
 % INPUTS:
 % --------
-% GEMpath = path to GEM mat file
+% GEMpath = path to GEM mat files 
+% numframes = number of frames in GEM
 % yavg = width of transect in alongshore direction (m)
 % dxy = matrix bin size
 % ypick = transect location [x y] format
@@ -22,21 +23,65 @@ xloc=239737;
 yloc=3784751;
 rotang=35;
 iyavg=round(yavg/dxy/2); % indices to pull out before and after transect to average over
+figfolder=figpath;
 
-% load GEM elevation values
-GEM=load(GEMpath);
+% Load GEM
+GEMz=load(GEMpath);
+GEMz=GEMz.meanGEMz;
 
-% create and rotate grid
+% GEM name
+GEMname=split(GEMpath,'/');
+GEMname=GEMname(9,1);
+GEMname=string(GEMname);
+GEMdate=datetime(str2num(GEMname),'ConvertFrom','epochtime','TicksPerSecond',1000);
+GEMdate=string(GEMdate);
+GEMtitle=append(GEMname,',',GEMdate);
+
+
+% create and rotate grid 
 gridX=0:dxy:110;
 gridY=-80:dxy:25;
 [x,y]=meshgrid(gridX,gridY);
-[xrot,yrot]=rotateCoordinates(GEM(:,1),GEM(:,2),Xloc,Yloc,rotang);
-
-% load GEM elevation values
-GEM=load(GEMpath);
 
 [~,iy]=min(abs(y(:,1)-ypick(1)));
-ztran=median(GEM(iy-iyavg:iy+iyavg,:,:),1,'omitnan');
+ztran=median(GEMz(iy-iyavg:iy+iyavg,:,:),1,'omitnan');
 ztran=squeeze(ztran);
-ztran=movemean(ztran,2,1,'omitnan');
+ztran=movmean(ztran,2,1,'omitnan');
+
 save(savepath,'ztran','-mat');
+
+% plotting specs 
+ftsz = [20 16];
+lw = 1.5;
+ixlim = [-120,0];
+iylim = [-100,0];
+iclim = [-0.201 0.201];
+tickminor = 'on';
+tickdir ='in';
+ticklen = 0.5;
+xlab = 'Cross-shore (m)';
+ylab = 'Elev. (m)';
+
+% plot and save for each frame
+for j=1:numframes
+    fig=figure('units','inches','position',[1 1 10 3],'color','w');
+    hold on
+    plot(x(1,:),ztran(:,j),'LineWidth',1.5);
+    box on
+    clim([0 (numframes-1)/2])
+    %colormap(cmap)
+    %h1 = plotstyleCMB(gca,xlab,ylab,ftsz,ticklen,lw,tickminor,tickdir);
+    %hc = colorbar('Location','eastoutside','Position', [0.93 0.225 0.03 0.4],'orientation','vertical','YAxisLocation','right');
+    %set(hc,'fontsize',ftsz(2),'linewidth',lw);
+    text(164,3.3,'$t$ (s)','interpreter','latex','fontsize',ftsz(1));
+
+    sname=append(GEMname,'_tran_',string(j));
+    figname=append(figfolder,'/',sname,'.png');
+    exportgraphics(fig,figname);
+    close(fig)
+end 
+
+
+
+
+
