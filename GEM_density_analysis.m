a0456254afcf5f860a1b028e0346c7da05c1bb32
bgaenzle <bagaenzl@ncsu.edu>
2025-06-26 11:44:24 -0400

    plotting (need to fix tick label errors)

--- GEM_density_analysis.m
+++ GEM_density_analysis.m
@@ -35,11 +35,30 @@ density=zeros(length(epochstring),length(regions)); % preallocate density matrix
 for k=1:length(epochstring)
     % load GEM mat file
     GEMpath=string(epochstring(k));
-    figpath=append(path,'Figures'); 
+    figpath=append(path,'Figures');
+    % save GEM name
+    GEMname=split(GEMpath,'/');
+    GEMname=GEMname(9,1);
+    GEMnames(k)=string(GEMname);
     for j=1:length(regions)
         region=regions(j);
         % calculate density and save for each GEM
-        density(i,j)=GEM_region_density_calc(GEMpath,region,figpath);
+        density(k,j)=GEM_region_density_calc(GEMpath,region,figpath);
     end
 end
 
+% plot density histogram
+% --------------------------
+% add  row and column of zeros for plotting
+zc=zeros(size(density,1),1);
+density_wzeros=[density,zc];
+zr=zeros(1,size(density_wzeros,2));
+density_wzeros=[zr;density_wzeros];
+
+[x,y]=meshgrid(1:length(regions)+1,1:length(epochstring)+1);
+z=density_wzeros;
+fig=figure(1);
+clf; pcolor(x,y,z); hold on; 
+a=colorbar; clim([0 1]);a.Label.String='Normalized Density (-)';
+xticks([0.5 1.5 2.5 3.5 4.5]); yticks([0.5 1.5 2.5 3.5 4.5 5.5 6.5 7.5 8.5 9.5 10.5]); % need to generalize this
+yticklabels([0,GEMnames,0]);xticklabels([0,regions,0]); title('GEM Region Density');
