31fd5ffdc1686a58c27ff49628183326a65e9538
bgaenzle <bagaenzl@ncsu.edu>
2025-06-26 10:45:48 -0400

    iterate through files to grab GEMs

--- GEM_density_analysis.m
+++ GEM_density_analysis.m
@@ -10,27 +10,28 @@ metashape_path=append(CAM_analysispath,'/Metashape/'); % metashape GEMs
 GEMpath_meta=append(metashape_path,'/1723489201189/');
 GEMpath_meas=append(measured_path,'/1723489201189/');
 
+listofFiles=dir(measured_path);
+filenames = cell(length(listofFiles), 1); % Preallocate cell arra
 
-% plot GEM to ID regions
-    xlab = 'Cross-Shore (m)';ylab = 'Alongshore (m)';
-    fig=figure('units','inches','position',[0 0 10 6],'color','w');
-    pcolor(Xgrid,Ygrid,meanGEMz(:,:,1)); grid off;box on;hold on
-    %scatter(GCPx,GCPy,60,'fill','r','MarkerEdgeColor','k') (need to figure
-    %out how to specifiy GCPs/do we need?)
-    %scatter(CamAx,CamAy,60,'fill','sq','m','MarkerEdgeColor','k');
-    %text(CamAx(1)+0.5, CamAy(1), 'Cam A', 'FontSize', 12, 'Color', 'm');
-    %scatter(CamBx,CamBy,60,'fill','sq','m','MarkerEdgeColor','k');
-    %text(CamBx(1)+0.5, CamBy(1), 'Cam B', 'FontSize', 12, 'Color', 'm');
-    scatter(rbrx,rbry,60,'fill','sq','g','MarkerEdgeColor','k');
-    text(rbrx(1)+0.5, rbry(1), 'RBR', 'FontSize', 12, 'Color', 'g');
-    shading interp;
-    axis equal;ylim([-60 30]); ylabel(ylab);xlabel(xlab);xlim([-10 90]);clim([0 3.8]);
-    ftsz = [22 18]; lw = 1.2; hc = colorbar('Location','eastoutside','Position', [0.83 0.14 0.035 0.4],'orientation','vertical','YAxisLocation','right');
-    set(hc,'fontsize',ftsz(2),'linewidth',lw);
-    set(gcf, 'Units', 'Normalized', 'OuterPosition', [0, 0.04, 1, 0.96]); % Enlarge figure to full screen
-    %title(GEMtitle);
-    %filename=append('mean',GEMname,'_',string(i));
-    %meanfigpath=fullfile(figpath, filename);
-    %meanfigpath=append(figpath,"/mean",GEMname);
-    %saveas(fig,meanfigpath,'png');
-    %close(fig);
+for i=1:length(listofFiles)
+    baseFilename=listofFiles(i).name;
+    fullFilename=fullfile(listofFiles(i).folder,baseFilename);
+    filenames{i}=fullFilename;
+end
+
+% grab only GEM files (have at least one number in name)
+epochfile=regexp(filenames,'\d','once'); 
+epochfile=~cellfun('isempty',epochfile);
+epochstring=filenames(epochfile);
+
+for k=1:length(epochstring)
+    % load GEM mat file
+    GEMpath=string(append(epochstring(k),'/meanGEMz.mat'));
+    GEMz=load(GEMpath);
+    GEMz=GEMz.meanGEMz;
+    
+end
+
+
+
+figpath=append(measured_path,'Figures');
