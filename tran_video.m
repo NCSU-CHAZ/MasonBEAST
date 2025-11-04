63f7dadb733fa71c3250c8f0889bec9ebef464ec
bgaenzle <bagaenzl@ncsu.edu>
2025-11-04 14:31:05 -0500

    change GEM to MAP, edits to plotting. quality plot not saving as png

--- tran_video.m
+++ tran_video.m
@@ -1,5 +1,5 @@
-function[v,quality_array]=tran_video(GEMmatrixpath,yavg,dxy,ypick,figpath)
-%function[wave_video]=tran_video(ztranpath)
+function[v,quality_array]=tran_video(MAPzmatrixpath,yavg,dxy,ypick,figpath)
+%function[v,quality_array]=tran_video(MAPzmatrixpath,yavg,dxy,ypick,figpath)
 % 
 % This function takes in a GEM and creates a video showing timesteps of 1 
 % second a specified transect. The lowest transect values are used as 
@@ -9,7 +9,7 @@ function[v,quality_array]=tran_video(GEMmatrixpath,yavg,dxy,ypick,figpath)
 % 
 % INPUTS:
 % ---------
-% GEMmatrixpath = path to GEM mat files 
+% MAPzmatrixpath = path to MAP elevation mat file matrix
 % yavg = width of transect in alongshore direction (m)
 % dxy = matrix bin size
 % ypick = transect location [x y] format
@@ -24,6 +24,7 @@ function[v,quality_array]=tran_video(GEMmatrixpath,yavg,dxy,ypick,figpath)
 % EDITS NEEDED
 % -------------
 % 
+% Last Updated: 11/4/2025 BG
 
 % Constants
 xloc=239737;
@@ -33,18 +34,18 @@ iyavg=round(yavg/dxy/2); % indices to pull out before and after transect to aver
 figfolder=figpath; % path to save figures
 timestepfolder=append(figpath,'/Individual_timesteps'); % folder to save individual timestep pngs
 
-% Load GEM
+% Load elevation map
 
-GEMz=load(GEMmatrixpath);
-GEMz=GEMz.meanGEMz;
+MAPz=load(MAPzmatrixpath);
+MAPz=MAPz.meanMAPz;
 
 % GEM name
-GEMname=split(GEMmatrixpath,'/');
-GEMname=GEMname(8,1);
-GEMname=string(GEMname);
-GEMdate=datetime(str2num(GEMname),'ConvertFrom','epochtime','TicksPerSecond',1000);
-GEMdate=string(GEMdate);
-GEMtitle=append(GEMname,',',GEMdate);
+MAPname=split(MAPzmatrixpath,'/');
+MAPname=MAPname(8,1);
+MAPname=string(MAPname);
+MAPdate=datetime(str2num(MAPname),'ConvertFrom','epochtime','TicksPerSecond',1000);
+MAPdate=string(MAPdate);
+MAPtitle=append(MAPname,',',MAPdate);
 
 % create and rotate grid 
 gridX=0:dxy:110;
@@ -53,7 +54,7 @@ gridY=-80:dxy:25;
 
 % extranct transect in all frames
 [~,iy] = min(abs(y(:,1)-ypick(1)));
-ztran = median(GEMz(iy-iyavg:iy+iyavg,:,:),1,'omitnan');
+ztran = median(MAPz(iy-iyavg:iy+iyavg,:,:),1,'omitnan');
 ztran = squeeze(ztran); % one matrix
 ztran = movmean(ztran,2,1,'omitnan');
 
@@ -91,7 +92,8 @@ ticklen = 0.5;
 xlab = 'Cross-shore (m)';
 ylab = 'Elev. (m)';
 
-sname =append(GEMname,'_timeseries');
+sname =append(MAPname,'_',string(ypick(1)),'_timeseries_',string(yavg));
+% this is epochnum_transect y location_timeseries_yavg
 
 % need to test this
 v = VideoWriter(append(figfolder,'\',sname), 'MPEG-4');
@@ -103,18 +105,19 @@ figure('units','inches','position',[1 1 10 3],'color','w');
 
 for i = 1:numframes %+1
     clf
-    plot(x(1,ixon:ix),zbeach(ixon:ix),'LineWidth',3,'Color',[148, 116, 27]/256) % plots beach (minimum transect)
+    plot(x(1,ixon:ix)*dxy,zbeach(ixon:ix),'LineWidth',3,'Color',[148, 116, 27]/256) % plots beach (minimum transect)
     hold on
-    plot(x(1,ix:end),ztran(ix:end,i),'LineWidth',3,'Color','b') % plots water in swash
-    plot(x(1,ixtran:ix),ztran(ixtran:ix,i),'LineWidth',3,'Color','b') % plots water in uprush
+    plot(x(1,ix:end)*dxy,ztran(ix:end,i),'LineWidth',3,'Color','b') % plots water in swash
+    plot(x(1,ixtran:ix)*dxy,ztran(ixtran:ix,i),'LineWidth',3,'Color','b') % plots water in uprush
 
     box on
     ylim([0 4.5]);
-    xlim([0 100]);
+    xlim([0 23]);
     clim([0 (numframes-1)/2])
     %colormap(cmap)
     %h1 = plotstyleCMB(gca,xlab,ylab,ftsz,ticklen,lw,tickminor,tickdir);
     title(['$t$ = ',num2str(round(i/2)),' s'],'interpreter','latex','fontsize',ftsz(1));
+    xlabel("Cross-Shore (m)",'Fontsize',14,'Fontname','Times New Roman'); ylabel("Elevation (m)",'Fontsize',14,'Fontname','Times New Roman')
     pause(0.1)
     writeVideo(v,getframe(gcf))
 end
@@ -124,7 +127,7 @@ close(v)
 % quality
 for i = 1:numframes%+1
     clf
-    fig=figure(1);
+    fig=figure('units','inches','position',[1 1 10 3],'color','w');
     plot(x(1,ixon:ix),zbeach(ixon:ix),'LineWidth',3,'Color',[148, 116, 27]/256) % plots beach (minimum transect)
     hold on
     plot(x(1,ix:end),ztran(ix:end,i),'LineWidth',3,'Color','b') % plots water in swash
@@ -137,8 +140,9 @@ for i = 1:numframes%+1
     %colormap(cmap)
     %h1 = plotstyleCMB(gca,xlab,ylab,ftsz,ticklen,lw,tickminor,tickdir);
     title(['$t$ = ',num2str(round(i/2)),' s'],'interpreter','latex','fontsize',ftsz(1));
+    xlabel("Cross-Shore (m)",'Fontsize',14,'Fontname','Times New Roman'); ylabel("Elevation (m)",'Fontsize',14,'Fontname','Times New Roman')
 
-    filename=append('timestep_',num2str(round(i/2)));
+    filename=append(MAPname,'_',string(ypick(1)),'_',string(yavg),'_timestep_',num2str(round(i/2)));
     timefigpath=fullfile(timestepfolder, filename);
     
     nannum=sum(isnan(ztran(:,i)));
@@ -160,16 +164,18 @@ for i = 1:numframes%+1
    
 end
 
-fig=figure(3);clf;
-plot(quality_array,'o','Color','m','MarkerFaceColor','m','MarkerSize',8); hold on;
-yline(0.05,'LineStyle','-','Color','k','Linewidth',2,'DisplayName','0.05 Threshold');
-hold on; legend('Quality Value (# NaNs/# points in transect)','0.05 Threshold'); xlabel('Timestep #');
+fig=figure('units','inches','position',[1 1 7 5],'color','w');clf;
+plot(quality_array,'o','Color','m','MarkerFaceColor','m','MarkerSize',6); hold on;
+%yline(0.05,'LineStyle','-','Color','k','Linewidth',2,'DisplayName','0.05 Threshold');
+hold on; legend('Quality Value (# NaNs/# points in transect)','0.05 Threshold'); xlabel('Image # (time/2)');
 ylim([0 1]);xlim([0 numframes+1]); ax=gca; ax.XTick=unique(round(ax.XTick));title('Quality Value for Transects');
-qualfigpath=append(figfolder,'/QualityofTransects');
-saveas(fig,qualfigpath,'png');
-matname=fullfile(qualfigpath,append('/quality','.mat'));
-save(matname,'quality_array');
+qualfigpath=append(figfolder,'/Quality_of_Transects');
+qualname=append(qualfigpath,'/qaulityPlot',string(ypick(1)),'_',string(yavg));
+saveas(fig,qualname,'png');
 close(fig);
+
+matname=fullfile(qualfigpath,append('/quality','_',string(ypick(1)),'_',string(yavg),'.mat'));
+save(matname,'quality_array');
 
 %% TEST
 % GEMpath='/Volumes/kanarde/MasonBEAST/data/GEMs/Camera_Location_Analysis/Measured/1708030441768/meanGEMz.mat';
